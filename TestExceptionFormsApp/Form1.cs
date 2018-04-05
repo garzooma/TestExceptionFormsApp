1c83c91ae42fd56883a23cd25e649c5efac6ca2e
garzooma <greg@arzoosoftware.com>
2018-04-05 15:31:28 -0400

    Init commit
    INit commit

---
+++ TestExceptionFormsApp/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestExceptionFormsApp
+{
+  public partial class Form1 : Form {
+    public Form1() {
+      InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e) {
+      Console.WriteLine("Throw test exception");
+      throw new Exception("Test Exception");
+    }
+
+    private void button2_Click(object sender, EventArgs e) {
+      ParameterizedThreadStart threadDelegate = new ParameterizedThreadStart(ThreadMethod);
+      Thread thread = new Thread(() => {
+        Console.WriteLine("Starting thread");
+        throw new Exception("Thread exception");
+      });
+      Action<Exception> handlerAction = exception => {
+        Console.WriteLine(String.Format("Handling Exception: {0}", exception));
+        throw exception;
+      };
+      try {
+        thread.Start(handlerAction);
+      }
+      catch (Exception excp) {
+        Console.Write("Caught thread exception");
+      }
+    }
+
+
+    private void ThreadMethod(Object obj) {
+      try {
+        Console.WriteLine("Starting thread");
+        throw new Exception("Thread exception");
+      }
+      catch (Exception excp) {
+        Action<Exception> handlerAction = obj as Action<Exception>;
+        handlerAction.Invoke(excp);
+      }
+    }
+  }
+}
---
+++ TestExceptionFormsApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestExceptionFormsApp
+{
+  static class Program
+  {
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+      Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+      Application.ThreadException += GUIErrorHandler;
+
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new Form1());
+    }
+
+    private static void GUIErrorHandler(object sender, ThreadExceptionEventArgs e)
+    {
+      MessageBox.Show(String.Format("Exception: {0}", e.Exception));
+    }
+  }
+}
